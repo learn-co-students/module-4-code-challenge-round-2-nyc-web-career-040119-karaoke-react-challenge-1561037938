236d41d80b290eabe4a551bfdda091d6c5b66c6e
David <davidbernier91@gmail.com>
2019-06-20 11:21:14 -0400

    mod4challenge

--- src/components/KaraokeDisplay.js
+++ src/components/KaraokeDisplay.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Lyrics from './Lyrics';
+import VoteBar from './VoteBar';
 
-const KaraokeDisplay = () => {
+const KaraokeDisplay = (props) => {
+  const song = (props.displaySong)
+
   return (
+    
+    <div>
+      <VoteBar
+      
+      />
     <div className="karaoke-display">
-      <h2>Song Title</h2>
-      <Lyrics lyrics="example song lyrics" />
+      <h2>{song.title}</h2>
+      <Lyrics lyrics={song.lyrics} />
+    </div>
     </div>
   )
 }
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import Song from '../components/Song';
 
-const SongList = () => {
+const SongList = (props) => {
+  const search = (props.filter)
+  const songs = (props.songs)
+
+  const filtered = songs.filter(song => (song.title.includes(search)))
+
+  const renderSongs = filtered.map(song=>
+    <Song
+    song={song}
+    key={song.key} 
+    playButton={props.playButton}
+    />
+    )
+
+
+
   return (
     <table className="song-list">
       <tbody>
         <tr>
           <th>Title</th>
           <th>Singer</th>
+          <th>Likes</th>
+          <th>Dislikes</th>
           <th>▶</th>
         </tr>
-
-        {/* Your Code Goes Here */}
-        
+        {renderSongs}
       </tbody>
     </table>
   )
